afd928f0d5c1363e8198d3e20d85381af9f3e945
Mnilsson183 <morgan.nilsson7111@icloud.com>
2023-02-24 17:49:51 -0700

    Commit

--- partner_get.py
+++ partner_get.py
@@ -1,6 +1,7 @@
 import httplib2
 import sqlite3
 from bs4 import BeautifulSoup
+from lxml.html import tostring, html5parser
 #estable http connection
 http = httplib2.Http()
 status, response = http.request('http://api.kivaws.org/v1/partners.html')
@@ -10,7 +11,7 @@ soup = BeautifulSoup(response)
 #soup = BeautifulSoup(response, "lxml")
 lines=[]
 # establish the connection to the sqlite db
-conn = sqlite3.connect('kiva')
+conn = sqlite3.connect('kiva.db')
 c = conn.cursor()
 c.execute('CREATE TABLE IF NOT EXISTS partners (id tinyint primary key, name varchar(255), status varchar(10), rating varchar(20), due_dilegence_type varchar(12), start_date datetime, delinquency_rate float, default_rate float, total_amount_raised unsigned bigint, loans_posted smallint, portfolio_yield real)');
 count = 0
@@ -20,6 +21,8 @@ for partner in soup.find_all('tr')[1:]:
 		# i dont want all the data so purposefully limit the rows
 		if(count < 11):
 			text=td.renderContents().decode('utf-8')
+			if text == ' ':
+				text = None
 			lines.append(text)
 		count += 1
 	c.execute('INSERT OR REPLACE INTO partners (id,name,status,rating,due_dilegence_type,start_date,delinquency_rate,default_rate,total_amount_raised,loans_posted,portfolio_yield) VALUES(?,?,?,?,?,?,?,?,?,?,?)', lines);
